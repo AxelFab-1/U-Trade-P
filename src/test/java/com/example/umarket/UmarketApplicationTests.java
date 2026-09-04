0a0f553ca180643fa91804724364ffceeff29082
AxelFab-1 <axelfabian987@gmail.com>
2026-09-04 17:10:11 -0500

    Commit inicial - estructura springboot

---
+++ src/test/java/com/example/umarket/UmarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.umarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UmarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
