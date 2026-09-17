57083a83650d3d96b0d78e6fc75035476019651f
yarlyan <yarly323@gmail.com>
2026-09-17 04:15:16 -0500

    feat(front): Crear vista 'Mensajes' y añadir su ruta

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,10 +1,13 @@
 import { Routes } from '@angular/router';
 import { Login } from './pages/login/login';
 import { Publicar } from './pages/publicar/publicar';
+import { Mensajes } from './pages/mensajes/mensajes';
 
 export const routes: Routes = [
   { path: 'login', component: Login },
-  { path: 'publicar', component: Publicar }
+  { path: 'publicar', component: Publicar },
+  { path: 'mensajes', component: Mensajes }
 ];
+
 
 
