ec9103abf97c95da3b04dc2a8fc3135c63dea9d4
yarlyan <yarly323@gmail.com>
2026-09-17 04:13:06 -0500

    feat(front): Crear vista 'Publicar' y añadir su ruta

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,7 +1,10 @@
 import { Routes } from '@angular/router';
 import { Login } from './pages/login/login';
+import { Publicar } from './pages/publicar/publicar';
 
 export const routes: Routes = [
-  { path: 'login', component: Login }
+  { path: 'login', component: Login },
+  { path: 'publicar', component: Publicar }
 ];
+
 
---
+++ frontend/src/app/pages/publicar/publicar.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-publicar',
+  imports: [],
+  templateUrl: './publicar.html',
+  styleUrl: './publicar.scss',
+})
+export class Publicar {
+
+}
