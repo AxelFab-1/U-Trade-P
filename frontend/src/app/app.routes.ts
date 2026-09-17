36da41c26806263f6a1a435974a0f1ec949dfb9c
yarlyan <yarly323@gmail.com>
2026-09-17 04:16:40 -0500

    feat(front): Crear vista 'Perfil Universitario' y añadir su ruta

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,12 +2,15 @@ import { Routes } from '@angular/router';
 import { Login } from './pages/login/login';
 import { Publicar } from './pages/publicar/publicar';
 import { Mensajes } from './pages/mensajes/mensajes';
+import { Perfil } from './pages/perfil/perfil';
 
 export const routes: Routes = [
   { path: 'login', component: Login },
   { path: 'publicar', component: Publicar },
-  { path: 'mensajes', component: Mensajes }
+  { path: 'mensajes', component: Mensajes },
+  { path: 'perfil', component: Perfil }
 ];
+
 
 
 
